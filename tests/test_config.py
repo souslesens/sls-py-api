d55f714db90f659dea280e71ff933ec80874fd2d
Xavier Garnier <xavier.garnier@logilab.fr>
2025-07-17 17:09:37 +0200

    test: remove TestSlsConfig

--- tests/test_config.py
+++ tests/test_config.py
@@ -5,35 +5,7 @@ from tempfile import gettempdir
 from unittest import TestCase
 from unittest.mock import patch
 
-from sls_api.config import SlsConfig, SlsConfigParser
-
-
-class TestSlsConfig(TestCase):
-    def setUp(self):
-        self.path = Path(gettempdir()).joinpath("sls_api_test")
-        self.path.mkdir(mode=0o755, exist_ok=False)
-
-    def tearDown(self):
-        if self.path.exists():
-            rmtree(self.path)
-
-    def test_construct_config_with_empty_files(self):
-        mainconfig = self.path.joinpath("mainConfig.json")
-        mainconfig.touch()
-
-        with self.assertRaises(JSONDecodeError):
-            SlsConfig(self.path)
-
-    def test_construct_config(self):
-        for filename in ("mainConfig", "sources", "profiles"):
-            config = self.path.joinpath(f"{filename}.json")
-            config.write_text("{}")
-
-        users = self.path.joinpath("users")
-        users.mkdir()
-        users.joinpath("users.json").write_text("{}")
-
-        SlsConfig(self.path)
+from sls_api.config import SlsConfigParser
 
 
 class TestSlsConfigParser(TestCase):
